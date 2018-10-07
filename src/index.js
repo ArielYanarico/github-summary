53f5cfecbd33292e0ef947e360f82ac120608e07
Ariel Yanarico <argos.yo@gmail.com>
2018-10-07 12:22:42 -0400

    Added: router and dummy routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
-import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import App from './components/app/App';
 
 ReactDOM.render(
-  <App />, 
+  <BrowserRouter basename='/home'>
+    <App />
+  </BrowserRouter>, 
   document.getElementById('root')
 );
 
